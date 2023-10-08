6545bd108787b426eb254d5306afc5e330515e64
Mmsshakil <softhandshakil@gmail.com>
2023-10-09 00:54:06 +0600

    loading problem solved

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,7 +4,15 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+
+    
+
+    if(loading){
+        return <div className="flex justify-center  h-[50vh]">
+            <span className="loading loading-spinner loading-lg text-accent"></span>
+        </div>;
+    }
 
     if(user){
         return children;
