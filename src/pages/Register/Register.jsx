49dfa3e9033e6bf9303d255cbc0107eefd759134
Mmsshakil <softhandshakil@gmail.com>
2023-10-09 16:08:09 +0600

    after registration the page will reload added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -54,6 +54,7 @@ const Register = () => {
                 })
                 .then(res =>{
                     console.log(res);
+                    window.location.reload();
                 })
                 .catch(error =>{
                     console.log(error.message);
